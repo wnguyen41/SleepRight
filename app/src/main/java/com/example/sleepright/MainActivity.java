5dc5cfa7669361ce7f8564b3f9f307fb55b7b2b4
Winston Nguyen <winstodn@uci.edu>
2021-02-09 20:18:08 -0800

    Integration with mongodb realm instead of atlas. Added quick start code for realm.

--- app/src/main/java/com/example/sleepright/MainActivity.java
+++ app/src/main/java/com/example/sleepright/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sleepright;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,7 +18,25 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.MongoClientSettings;
 
+import org.bson.types.ObjectId;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import io.realm.OrderedCollectionChangeSet;
+import io.realm.OrderedRealmCollectionChangeListener;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.sync.SyncConfiguration;
+
 public class MainActivity extends AppCompatActivity {
+    Realm uiThreadRealm;
+    App app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +51,110 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        // Database stuff(don't actually knows where it goes rn. This is just test code
+        Realm.init(this);
+        String appID = "sleepright-venhq";
+        app = new App(new AppConfiguration.Builder(appID)
+                .build());
+
+        Credentials credentials = Credentials.anonymous();
+        app.loginAsync(credentials, result -> {
+            if (result.isSuccess()) {
+                Log.v("QUICKSTART", "Successfully authenticated anonymously.");
+                User user = app.currentUser();
+                String partitionValue = "My Project";
+                SyncConfiguration config = new SyncConfiguration.Builder(
+                        user,
+                        partitionValue)
+                        .build();
+                uiThreadRealm = Realm.getInstance(config);
+                addChangeListenerToRealm(uiThreadRealm);
+                FutureTask<String> task = new FutureTask(new BackgroundQuickStart(app.currentUser()), "test");
+                ExecutorService executorService = Executors.newFixedThreadPool(2);
+                executorService.execute(task);
+            } else {
+                Log.e("QUICKSTART", "Failed to log in. Error: " + result.getError());
+            }
+        });
     }
 
-    ConnectionString connString = new ConnectionString(
-            "mongodb+srv://<sleepright>:<password>@<cluster-address>/Cluster0?w=majority"
-    );
+    private void addChangeListenerToRealm(Realm realm) {
+        // all tasks in the realm
+        RealmResults<Task> tasks = uiThreadRealm.where(Task.class).findAllAsync();
+        tasks.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<Task>>() {
+            @Override
+            public void onChange(RealmResults<Task> collection, OrderedCollectionChangeSet changeSet) {
+                // process deletions in reverse order if maintaining parallel data structures so indices don't change as you iterate
+                OrderedCollectionChangeSet.Range[] deletions = changeSet.getDeletionRanges();
+                for (OrderedCollectionChangeSet.Range range : deletions) {
+                    Log.v("QUICKSTART", "Deleted range: " + range.startIndex + " to " + (range.startIndex + range.length - 1));
+                }
+                OrderedCollectionChangeSet.Range[] insertions = changeSet.getInsertionRanges();
+                for (OrderedCollectionChangeSet.Range range : insertions) {
+                    Log.v("QUICKSTART", "Inserted range: " + range.startIndex + " to " + (range.startIndex + range.length - 1));                            }
+                OrderedCollectionChangeSet.Range[] modifications = changeSet.getChangeRanges();
+                for (OrderedCollectionChangeSet.Range range : modifications) {
+                    Log.v("QUICKSTART", "Updated range: " + range.startIndex + " to " + (range.startIndex + range.length - 1));                            }
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // the ui thread realm uses asynchronous transactions, so we can only safely close the realm
+        // when the activity ends and we can safely assume that those transactions have completed
+        uiThreadRealm.close();
+        app.currentUser().logOutAsync(result -> {
+            if (result.isSuccess()) {
+                Log.v("QUICKSTART", "Successfully logged out.");
+            } else {
+                Log.e("QUICKSTART", "Failed to log out, error: " + result.getError());
+            }
+        });
+    }
+    public class BackgroundQuickStart implements Runnable {
+        User user;
+        public BackgroundQuickStart(User user) {
+            this.user = user;
+        }
+        @Override
+        public void run() {
+            String partitionValue = "My Project";
+            SyncConfiguration config = new SyncConfiguration.Builder(
+                    user,
+                    partitionValue)
+                    .build();
+            Realm backgroundThreadRealm = Realm.getInstance(config);
+            Task task = new Task("New Task");
+            backgroundThreadRealm.executeTransaction (transactionRealm -> {
+                transactionRealm.insert(task);
+            });
+            // all tasks in the realm
+            RealmResults<Task> tasks = backgroundThreadRealm.where(Task.class).findAll();
+            // you can also filter a collection
+            RealmResults<Task> tasksThatBeginWithN = tasks.where().beginsWith("name", "N").findAll();
+            RealmResults<Task> openTasks = tasks.where().equalTo("status", TaskStatus.Open.name()).findAll();
+            Task otherTask = tasks.get(0);
+            // all modifications to a realm must happen inside of a write block
+            backgroundThreadRealm.executeTransaction( transactionRealm -> {
+                Task innerOtherTask = transactionRealm.where(Task.class).equalTo("_id", otherTask.get_id()).findFirst();
+                innerOtherTask.setStatus(TaskStatus.Complete);
+            });
+            Task yetAnotherTask = tasks.get(0);
+            ObjectId yetAnotherTaskId = yetAnotherTask.get_id();
+            // all modifications to a realm must happen inside of a write block
+            backgroundThreadRealm.executeTransaction( transactionRealm -> {
+                Task innerYetAnotherTask = transactionRealm.where(Task.class).equalTo("_id", yetAnotherTaskId).findFirst();
+                innerYetAnotherTask.deleteFromRealm();
+            });
+            // because this background thread uses synchronous realm transactions, at this point all
+            // transactions have completed and we can safely close the realm
+            backgroundThreadRealm.close();
+        }
+    }
 
-    MongoClientSettings settings = MongoClientSettings.builder()
-            .applyConnectionString(connString)
-            .retryWrites(true)
-            .build();
-    MongoClient mongoClient = MongoClients.create(settings);
-    MongoDatabase database = mongoClient.getDatabase("users");
+
+
+
 }
---
+++ app/src/main/java/com/example/sleepright/TaskStatus.java
@@ -0,0 +1,11 @@
+package com.example.sleepright;
+
+public enum TaskStatus {
+    Open("Open"),
+    InProgress("In Progress"),
+    Complete("Complete");
+    String displayName;
+    TaskStatus(String displayName) {
+        this.displayName = displayName;
+    }
+}
