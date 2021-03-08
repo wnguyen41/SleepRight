415c530bc8064961c8fd720c81d108428558a551
Winston Nguyen <winstodn@uci.edu>
2021-03-07 19:36:13 -0800

    Implemented log in and sign out functionality.

--- app/src/main/java/com/example/sleepright/GoogleLoginActivity.java
+++ app/src/main/java/com/example/sleepright/GoogleLoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.sleepright;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -13,11 +16,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class GoogleLoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private GoogleSignInClient mGoogleSignInClient;
+    private TextView mStatus, mImportCount, mLoggedAs;
+    private Button mSignOut, mGoogleSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,17 @@ public class GoogleLoginActivity extends AppCompatActivity implements View.OnCli
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mStatus = (TextView) findViewById(R.id.textView_status);
+        mImportCount = (TextView) findViewById(R.id.textView_import_count);
+        mLoggedAs = (TextView) findViewById(R.id.textView_logged_in_as);
+
+        mSignOut = (Button) findViewById(R.id.button_sign_out);
+        mGoogleSignIn = (Button) findViewById(R.id.sign_in_button);
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.button_cancel).setOnClickListener(this);
+        findViewById(R.id.button_import).setOnClickListener(this);
+        findViewById(R.id.button_sign_out).setOnClickListener(this);
     }
 
     @Override
@@ -42,8 +57,30 @@ public class GoogleLoginActivity extends AppCompatActivity implements View.OnCli
             case R.id.sign_in_button:
                 signIn();
                 break;
+            case R.id.button_cancel:
+                cancel();
+                break;
+            case R.id.button_import:
+                //importData();
+                break;
+            case R.id.button_sign_out:
+                signOut();
+                break;
             // ...
         }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, task -> {
+                    mLoggedAs.setText("Logged in as:");
+                    mSignOut.setEnabled(false);
+                    mGoogleSignIn.setEnabled(true);
+                });
+    }
+
+    private void cancel() {
+        finish();
     }
 
     @Override
@@ -92,9 +129,14 @@ public class GoogleLoginActivity extends AppCompatActivity implements View.OnCli
     public void updateUI(GoogleSignInAccount account){
         // TODO: This is temporary
         if(account != null){
+            mLoggedAs.setText("Logged in as: " + account.getGivenName());
+            mGoogleSignIn.setEnabled(false);
+            mSignOut.setEnabled(true);
             Toast.makeText(this,"Google Sign In Successful",Toast.LENGTH_LONG).show();
 
         }else {
+            mGoogleSignIn.setEnabled(true);
+            mSignOut.setEnabled(false);
             Toast.makeText(this,"GoogleSignIn: Null",Toast.LENGTH_LONG).show();
         }
 
